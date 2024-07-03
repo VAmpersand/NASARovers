59a6b5d89b50d8c407edafbe729dbbebf16c70a0
Viktor <viktor.prikolota@exinity.com>
2024-07-03 11:29:31 -0300

    Added config, endpointProvider, apiDataProvider

--- NASARovers/ContentView.swift
+++ NASARovers/ContentView.swift
@@ -8,6 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    let apiDataProvider = APIDataProvider()
+
+    init() {
+        apiDataProvider.getData(for: .manifests(rover: .curiosity)) { (data: Data) in
+            print(data.count)
+        } errorHandler: { error in
+            print(error.description)
+        }
+    }
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
---
+++ NASARovers/Models/Rover.swift
@@ -0,0 +1,14 @@
+//
+//  Rover.swift
+//  NASARovers
+//
+//  Created by Viktor Prikolota on 03.07.2024.
+//
+
+import Foundation
+
+enum Rover: String {
+    case curiosity
+    case opportunity
+    case spirit
+}
